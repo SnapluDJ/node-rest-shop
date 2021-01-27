a9175694babc6a5e8bd11eae0a7ebda080d4d37d
Dongjie <dongjielu@wustl.edu>
2021-01-28 00:21:43 +0800

    product route

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,16 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "get request of products",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -19,13 +26,17 @@ router.post("/", (req, res, next) => {
 
   product
     .save()
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err));
-
-  res.status(200).json({
-    message: "post request of products",
-    data: product,
-  });
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "post request of products",
+        data: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get("/:productId", (req, res, next) => {
@@ -33,20 +44,49 @@ router.get("/:productId", (req, res, next) => {
 
   Product.findById(id)
     .exec()
-    .then((log) => console.log(log))
-    .catch((err) => console.log(err));
+    .then((doc) => {
+      console.log(doc);
+
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "product not found",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "update product",
-  });
+  const id = req.params.productId;
+  Product.updateOne({ _id: id }, { price: req.body.price })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "delete product",
-  });
+  const id = req.params.productId;
+
+  Product.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
