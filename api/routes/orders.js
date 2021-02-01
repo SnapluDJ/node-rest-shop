dc53fb782915f6cd093d2769bb579b7b2d327345
Dongjie <dongjielu@wustl.edu>
2021-02-01 10:51:28 +0800

    order model & route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,34 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("../models/order");
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "order list",
-  });
+  Order.find()
+    .select("-__v")
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        orders: docs,
+      };
+
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity,
-  };
+  Product.findById(req.body.productId)
+    .exec()
+    .then(() => {
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId,
+      });
 
-  res.status(200).json({
-    message: "order created",
-    data: order,
-  });
+      return order.save();
+    })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
-router.get("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "order detail",
-  });
+router.get("/:orderId", async (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  try {
+    const order = await Order.findById(orderId).select("-__v");
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 router.delete("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "order deleted",
-  });
+  const id = req.params.orderId;
+
+  Order.findByIdAndDelete(id)
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
